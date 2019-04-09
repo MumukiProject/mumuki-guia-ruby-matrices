84c8ef15fcd83a4f1da75831764e05490603eca9
felipe@mumuki.org <felipe@mumuki.org>
2019-04-09 19:15:09 +0000

    Mumuki Export on 2019-04-09 19:15:09 +0000

--- 00005_Matriz revoluciones/default.rb
+++ 00005_Matriz revoluciones/default.rb
@@ -1,7 +1,8 @@
-def convertir_a_matriz_de_multiplicacion_5x5(matriz)
+def construir_matriz_de_multiplicacion_5x5
+  matriz = []
   for fila in 0..4
     for columna in 0..4
-       # matriz[fila][columna] = ...
+       #matriz[fila][columna] = ...
     end
   end
 end
